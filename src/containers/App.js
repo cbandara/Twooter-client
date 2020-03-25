30804c671fe5737350e1c838f513ce7605230452
Charutha Bandara <charuthabandara@gmail.com>
2020-03-25 18:33:33 -0400

    navbar styling and authform

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
-        Hello World!
+      <div className="onboarding">
+        <Navbar></Navbar>
+        <Main></Main>
       </div>
     </Router>
   </Provider>
