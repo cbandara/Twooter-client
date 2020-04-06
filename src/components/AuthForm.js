8fe2c47c2011206f647a0b8296ed78f12cdf7b1a
Charutha Bandara <charuthabandara@gmail.com>
2020-04-06 00:33:34 -0400

    successful login authenticated

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export default class AuthForm extends Component {
+class AuthForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -8,26 +9,36 @@ export default class AuthForm extends Component {
       username: "",
       password: "",
       profileImageUrl: ""
-    }
+    };
   }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     const authType = this.props.signUp ? "signup" : "signin";
-    this.props.onAuth(authType, this.state).then(() => {
-      return console.log("LOGGED IN");
-    })
-  }
+    this.props
+      .onAuth(authType, this.state)
+      .then(() => {
+        this.props.history.push("/");
+      })
+      .catch(() => {
+        return;
+      });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   render() {
     const { email, username, password, profileImageUrl } = this.state;
-    const { heading, buttonText, signUp, errors, history, removeError } = this.props;
+    const {
+      signUp,
+      heading,
+      buttonText,
+      errors,
+      history,
+      removeError
+    } = this.props;
 
     history.listen(() => {
       removeError();
@@ -39,25 +50,65 @@ export default class AuthForm extends Component {
           <div className="col-md-6">
             <form onSubmit={this.handleSubmit}>
               <h2>{heading}</h2>
-              {errors.message && <div className="alert alert-danger">{errors.message}</div>}
-              <label htmlFor="email">Email:</label>
-              <input className="form-control" id="email" name="email" onChange={this.handleChange} value={email} type="text"></input>
-              <label htmlFor="password">Password:</label>
-              <input className="form-control" id="password" name="password" onChange={this.handleChange} type="password"></input>
+              {errors.message && (
+                <div className="alert alert-danger">{errors.message}</div>
+              )}
+              <label htmlFor="email">E-mail</label>
+              <input
+                autoComplete="off"
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                type="text"
+                value={email}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                autoComplete="off"
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+              />
               {signUp && (
                 <div>
-                  <label htmlFor="username">Username:</label>
-                  <input className="form-control" id="username" name="username" onChange={this.handleChange} value={username} type="text"></input>
-                  <label htmlFor="image-url">Profile Image URL:</label>
-                  <input className="form-control" id="image-url" name="profileImageUrl" onChange={this.handleChange} type="text" value={profileImageUrl}></input>
+                  <label htmlFor="username">Username</label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={username}
+                  />
+                  <label htmlFor="image-url">Image URL</label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    id="image-url"
+                    name="profileImageUrl"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={profileImageUrl}
+                  />
                 </div>
               )}
-              <button type="submit" className="btn btn-primary btn-block btn-lg">{buttonText}</button>
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-lg"
+              >
+                {buttonText}
+              </button>
             </form>
           </div>
         </div>
       </div>
-
-    )
+    );
   }
 }
+
+export default AuthForm;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -6,23 +6,24 @@ export function setCurrentUser(user) {
   return {
     type: SET_CURRENT_USER,
     user
-  }
+  };
 }
 
 export function authUser(type, userData) {
   return dispatch => {
+    // wrap our thunk in a promise so we can wait for the API call
     return new Promise((resolve, reject) => {
       return apiCall("post", `/api/auth/${type}`, userData)
-        .then((token, ...user) => {
-          localStorage.setItem("jwtToken", token)
-          dispatch(setCurrentUser(user))
+        .then(({ token, ...user }) => {
+          localStorage.setItem("jwtToken", token);
+          dispatch(setCurrentUser(user));
           dispatch(removeError());
-          resolve();
+          resolve(); // indicate that the API call succeeded
         })
         .catch(err => {
           dispatch(addError(err.message));
-          reject();
-        })
-    })
-  }
+          reject(); // indicate the API call failed
+        });
+    });
+  };
 }
